7eb1d81f251a94f25c914643c77a461c20c55968
Diadlo <polsha3@gmail.com>
2017-05-26 09:15:12 +0300

    Small refactoring

--- myglwidget.cpp
+++ myglwidget.cpp
@@ -3,6 +3,15 @@
 
 #include "myglwidget.h"
 
+#define ANGLE(x) 2 * M_PI * (x) / n
+#define POINT(x, r, z) { \
+    cos(ANGLE(x)) * (r), \
+    sin(ANGLE(x)) * (r), \
+    z }
+
+#define VERTEX(p) glVertex3f(p.x, p.y, p.z)
+#define NORM(p, q) glNormal3f((p.x + q.x) / 2, (p.y + q.y) / 2, (p.z + q.z) / 2)
+
 MyGLWidget::MyGLWidget(QWidget *parent)
     : QGLWidget(QGLFormat(QGL::SampleBuffers), parent)
 {
@@ -83,9 +92,9 @@ QSize MyGLWidget::sizeHint() const
 static void qNormalizeAngle(int &angle)
 {
     while (angle < 0)
-        angle += 360 * 16;
+        angle += 360 * 2;
     while (angle > 360)
-        angle -= 360 * 16;
+        angle -= 360 * 2;
 }
 
 void MyGLWidget::setXRotation(int angle)
@@ -123,7 +132,6 @@ void MyGLWidget::initializeGL()
     qglClearColor(Qt::black);
 
     glEnable(GL_DEPTH_TEST);
-    glShadeModel(GL_SMOOTH);
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
 
@@ -136,9 +144,9 @@ void MyGLWidget::paintGL()
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
     glTranslatef(0.0, 0.0, -10.0);
-    glRotatef(xRot / 16, 1.0, 0.0, 0.0);
-    glRotatef(yRot / 16, 0.0, 1.0, 0.0);
-    glRotatef(zRot / 16, 0.0, 0.0, 1.0);
+    glRotatef(xRot / 2, 1.0, 0.0, 0.0);
+    glRotatef(yRot / 2, 0.0, 1.0, 0.0);
+    glRotatef(zRot / 2, 0.0, 0.0, 1.0);
     draw();
 }
 
@@ -163,18 +171,13 @@ void MyGLWidget::mouseMoveEvent(QMouseEvent *event)
     int dx = event->x() - lastPos.x();
     int dy = event->y() - lastPos.y();
 
-    if (event->buttons() & Qt::LeftButton) {
-        setXRotation(xRot + 8 * dy);
-        setYRotation(yRot + 8 * dx);
-    } else if (event->buttons() & Qt::RightButton) {
-        setXRotation(xRot + 8 * dy);
-        setZRotation(zRot + 8 * dx);
-    }
+    setXRotation(xRot + dy);
+    setYRotation(yRot + dx);
 
     lastPos = event->pos();
 }
 
-struct Point {
+struct Vertex {
     double x, y, z;
 };
 
@@ -183,26 +186,18 @@ void MyGLWidget::draw()
     void (*f)(GLenum cap) = transp ? glDisable : glEnable;
     f(GL_DEPTH_TEST);
 
-    QVector<Point> base, header;
-#define ANGLE(x) 2 * M_PI * (x) / n
-#define POINT(x, r, z) { \
-    cos(ANGLE(x)) * (r), \
-    sin(ANGLE(x)) * (r), \
-    z }
-
+    QVector<Vertex> base, header;
     base.push_back(POINT(0, r1, 0));
     header.push_back(POINT(0, r2, h));
     for (int i = 1; i <= n; i++) {
-        const Point& p1 = base.last();
-        Point p2 = POINT(i, r1, 0);
+        const Vertex& p1 = base.last();
+        Vertex p2 = POINT(i, r1, 0);
         base.push_back(p2);
 
-        const Point& p3 = header.last();
-        Point p4 = POINT(i, r2, h);
+        const Vertex& p3 = header.last();
+        Vertex p4 = POINT(i, r2, h);
         header.push_back(p4);
 
-#define VERTEX(p) glVertex3f(p.x, p.y, p.z)
-#define NORM(p, q) glNormal3f((p.x + q.x) / 2, (p.y + q.y) / 2, (p.z + q.z) / 2)
         glBegin(GL_QUADS);
         {
             NORM(p1, p2);
@@ -216,14 +211,14 @@ void MyGLWidget::draw()
 
     glBegin(GL_POLYGON);
         glNormal3f(0, 0, 1);
-        for (const Point& p : header) {
+        for (const Vertex& p : header) {
             VERTEX(p);
         }
     glEnd();
 
     glBegin(GL_POLYGON);
         glNormal3f(0, 0, -1);
-        for (const Point& p : base) {
+        for (const Vertex& p : base) {
             VERTEX(p);
         }
     glEnd();
